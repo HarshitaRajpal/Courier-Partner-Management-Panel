3e6f1b3c6f3ff2ab7e0a37877422a53b7fef0afe
HarshitaRajpal <harshitarajpal1523@gmail.com>
2025-11-02 19:48:45 +0530

    *inital setup done --fe
    *added tailwind, antd

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold">
+        testing...
+        <Button type="primary">testttt antsdd</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
