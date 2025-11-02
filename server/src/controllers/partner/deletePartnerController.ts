c7f6220dfb3cf3d65e4027c2bfe8418661dd1f2e
HarshitaRajpal <harshitarajpal1523@gmail.com>
2025-11-03 02:01:00 +0530

    *updated logic to delete and update partner

--- server/src/controllers/partner/deletePartnerController.ts
+++ server/src/controllers/partner/deletePartnerController.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
+import { PartnerService } from "../../services/partnerService";
 
-export const deletePartnerController = async (
-  req: Request,
-  res: Response
-) => {};
+export const deletePartnerController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await PartnerService.deletePartner(id);
+    res.json({ message: "Partner deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed To delete partner" });
+  }
+};
