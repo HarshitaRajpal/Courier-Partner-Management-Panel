bcc531b6f8b8e1f3e044f297e39afcb7286504bb
HarshitaRajpal <harshitarajpal1523@gmail.com>
2025-11-02 22:54:20 +0530

    *added migrations and updated createpartner controller
    *made phone number unique and rating optional

---
+++ server/prisma/migrations/20251102171912_made/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Partner" ALTER COLUMN "rating" DROP NOT NULL,
+ALTER COLUMN "rating" SET DEFAULT 0;
--- server/src/controllers/partner/createPartnerController.ts
+++ server/src/controllers/partner/createPartnerController.ts
@@ -3,10 +3,6 @@ import { PartnerService } from "../../services/partnerService";
 
 export const createPartnerController = async (req: Request, res: Response) => {
   try {
-    const { name, email, phone, baseRate, deliverySpeed } = req.body;
-    if (!name || !email || !phone || !baseRate || !deliverySpeed) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
     const partner = await PartnerService.createPartner(req.body);
 
     res.status(201).json({
@@ -14,6 +10,6 @@ export const createPartnerController = async (req: Request, res: Response) => {
       data: partner,
     });
   } catch (err) {
-    res.status(500).json({ message: "Failed To create partner" });
+    res.status(500).json({ message: "Failed To create partner", error: err });
   }
 };
