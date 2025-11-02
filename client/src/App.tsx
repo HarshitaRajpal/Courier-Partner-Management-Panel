a6b29b92c3c66a3cb766c4d32663b531aab3632e
HarshitaRajpal <harshitarajpal1523@gmail.com>
2025-11-03 00:07:34 +0530

    *added tanstack query and axios --instance created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function App() {
-  return <RouterProvider router={router} />;
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
-
-export default App;
